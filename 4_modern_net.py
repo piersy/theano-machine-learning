9e86479b68eab70cab332846af9936f5d74560b4
piersy <pierspowlesland@gmail.com>
2015-05-23 19:34:36 +0100

    About to start second layer visualisation for 3_net.py

--- 4_modern_net.py
+++ 4_modern_net.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 import matplotlib.pyplot as plt
 import theano
 from theano import tensor as T
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
-import numpy as np
+
 from load import mnist
+
 
 srng = RandomStreams()
 
@@ -92,59 +95,37 @@ updates = RMSprop(cost, params, lr=0.001)
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
 
-for i in range(2):
+for i in range(10):
     for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
-    # for start, end in zip(range(0, len(trX) / 20, 128), range(128, len(trX) / 20, 128)):
+        # for start, end in zip(range(0, len(trX) / 20, 128), range(128, len(trX) / 20, 128)):
         cost = train(trX[start:end], trY[start:end])
     print np.mean(np.argmax(teY, axis=1) == predict(teX))
 
+# First layer
+# Pick an input and find the first layer intermediate state for that input
+# find the most activated units of the first layer and then display those units
+# purely by themselves and after activation from the input
+
+# Second layer
+# Pick an input and find the second layer intermediate state for that input
+# find the most activated units of the second layer and for each unit of the
+# second layer display the the sum of all images from the first layer after
+# rectification each multiplied by the input weight of the second layer
+
 intermediate = teX[0, :].reshape((1, 784)).transpose() * w_h.get_value()
-
-# max = intermediate.max()
-# min = intermediate.min()
-#
-# print "min:"+min.astype('str')
-# print "max:"+max.astype('str')
-
-# intermediate += np.absolute(min)
-
-print "max:" + intermediate.max().astype('str')
-print "min:" + intermediate.min().astype('str')
-print intermediate.shape
-
-
-# I find the intermediates state sum to get intermediate activation values then plug that into the w_h and argsort
-
-
-# add row of indices to bottom of matrix so that when sorted we know the index to look back into the w_h
-# intermediate = np.append(intermediate, [np.arange(intermediate.shape[1])], 0)
-# print "With indexes:", intermediate.shape
-
-# print "Without indexes:", intermediate[:-1, :].shape
-
-# print "Sum without indexes", np.sum(intermediate[:-1, :], axis=1)
-
-sumshape = np.sum(intermediate, axis=0)
-print "sum shape: ", sumshape.shape
-
+intermediate_activations = np.sum(intermediate, axis=0)
 # sort ith highest scoring columns first
-intermediate = normalize(intermediate[:, sumshape.argsort()[::-1]])
-w_h_value = normalize(w_h.get_value()[:, sumshape.argsort()[::-1]])
-# intermediate = intermediate[np.sum(intermediate[:-1, :], axis=1).argsort()]
-# intermediate /= intermediate.max()
-# print "sorted intermediate: ", intermediate.shape
-# plt.imshow(teX[0, :].reshape((28, 28)), cmap=plt.cm.gray, interpolation="nearest", vmin=0, vmax=1)
-# plt.show()
+intermediate = normalize(intermediate[:, intermediate_activations.argsort()[::-1]])
+w_h_value = normalize(w_h.get_value()[:, intermediate_activations.argsort()[::-1]])
 
+print "w_h_value max: ", np.max(intermediate)
+print "w_h_value min: ", np.min(intermediate)
 
-
-print "w_h_value max: ",  np.max(intermediate)
-print "w_h_value min: ",  np.min(intermediate)
 
 def add_to_plot(plot, matrix, amount_to_plot, plot_offset):
     for pos in range(amount_to_plot):
         print pos
-        sub = plt.subplot(height, width, pos+plot_offset+1)
+        sub = plot.subplot(height, width, pos + plot_offset + 1)
         sub.axis("off")
         sub.imshow(matrix[:, pos].reshape((28, 28)), cmap=plt.cm.gray, interpolation="none", vmin=0, vmax=1)
 
@@ -152,32 +133,8 @@ height = 8
 width = 5
 plot_amount = 10
 plt.figure(figsize=(height, width))
-add_to_plot(plt, w_h_value, plot_amount, plot_amount*0)
-add_to_plot(plt, intermediate, plot_amount, plot_amount*1)
-add_to_plot(plt, w_h_value[:, ::-1], plot_amount, plot_amount*2)
-add_to_plot(plt, intermediate[:, ::-1], plot_amount, plot_amount*3)
-# for i in range(width * height / 2):
-#     print "I: ", i
-#     sub = plt.subplot(height, width, i+1)
-#     sub.axis("off")
-#     sub.imshow(w_h_value[:, i].reshape((28, 28)), cmap=plt.cm.gray, interpolation="none", vmin=0, vmax=1)
-#
-# for i in range((width * height / 2)+1, (width * height)+1):
-#     print "new I: ", i
-#     sub = plt.subplot(height, width, i)
-#     sub.axis("off")
-#     sub.imshow(intermediate[:, i].reshape((28, 28)), cmap=plt.cm.gray, interpolation="none", vmin=0, vmax=1)
-
+add_to_plot(plt, w_h_value, plot_amount, plot_amount * 0)
+add_to_plot(plt, intermediate, plot_amount, plot_amount * 1)
+add_to_plot(plt, w_h_value[:, ::-1], plot_amount, plot_amount * 2)
+add_to_plot(plt, intermediate[:, ::-1], plot_amount, plot_amount * 3)
 plt.show()
-
-
-# height = 1
-# width = 10
-#
-# plt.figure(figsize=(height, width))
-# for i in range(width*height):
-# sub = plt.subplot(height, width, i)
-# t = (((w_o.get_value()[:, i].reshape((625, 1)) * np.transpose(w_h2.get_value())).sum(axis=0)).reshape((625, 1)) * np.transpose(w_h.get_value())).sum(axis=0)
-#     sub.axis("off")
-#     sub.imshow(t.reshape((28, 28)), cmap=plt.cm.gray, interpolation="nearest")
-# plt.show()
